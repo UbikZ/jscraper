ae7e08a1c272e46e38cbd2e38691ce34f42378d6
Gabriel Malet <gabrielmalet@gmail.com>
2017-01-25 20:14:23 +0100

    Test gradle build

---
+++ src/main/java/scraper/test/Hello.java
@@ -0,0 +1,10 @@
+package scraper.test;
+
+import org.joda.time.LocalTime;
+
+public class Hello {
+    public static void main(String[] args) {
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+    }
+}
