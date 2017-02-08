1855f25cba8dfd9cfd6597c14542513284215004
Gabriel Malet <gabrielmalet@gmail.com>
2017-02-08 14:41:06 +0100

    Fix user issue

--- jscraper/scripts/entrypoint.sh
+++ jscraper/scripts/entrypoint.sh
@@ -3,8 +3,8 @@
 # Wait database is online
 echo "[DEBUG] > Check database availability"
 java -jar /jscraper/database/build/libs/dbConnect-0.0.1.jar \
-    "$POSTGRES_HOST" "$POSTGRES_PORT" "$POSTGRES_NAME" "$POSTGRES_USERNAME" "$POSTGRES_PASSWORD" && \
-    exit 1
+    "$POSTGRES_HOST" "$POSTGRES_PORT" "$POSTGRES_NAME" "$POSTGRES_USER" "$POSTGRES_PASSWORD" || \
+     exit 1
 echo "[DEBUG] > Database is ready."
 
 # Launch server
