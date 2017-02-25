b05915c6b797822fc2a052f52d14525714ba5559
Gabriel Malet <gabrielmalet@gmail.com>
2017-02-25 17:25:51 +0100

    Add requestMapping for all controller

--- jscraper/scraper/src/main/java/com/ubikz/scraper/api/controller/AbstractController.java
+++ jscraper/scraper/src/main/java/com/ubikz/scraper/api/controller/AbstractController.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ubikz.scraper.core.app.service.message.BaseMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(value="/api")
+@RestController
 public class AbstractController {
     protected final Logger logger = LoggerFactory.getLogger(AbstractController.class);
 
