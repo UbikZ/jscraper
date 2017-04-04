7c001acd472cb686938b4476dec115f22a56488f
Gabriel Malet <gabrielmalet@gmail.com>
2017-04-04 11:23:19 +0200

    Test

--- jscraper/scraper/src/main/java/com/ubikz/scraper/core/middleware/CORSMiddleware.java
+++ jscraper/scraper/src/main/java/com/ubikz/scraper/core/middleware/CORSMiddleware.java
@@ -1,26 +1,24 @@
 package com.ubikz.scraper.core.middleware;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
-public class CorsMiddleware implements Filter {
+public class CORSMiddleware implements Filter {
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         chain.doFilter(req, res);
     }
 
-    public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {
+    }
 
-    public void destroy() {}
+    public void destroy() {
+    }
 }
