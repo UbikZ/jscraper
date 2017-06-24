e48d3d58af44c68e0914d2cfa507cde8ae59e2cc
Gabriel Malet <gabrielmalet@gmail.com>
2017-06-24 20:08:25 +0200

    Test

--- jscraper-ui/scripts/aws-ci.sh
+++ jscraper-ui/scripts/aws-ci.sh
@@ -6,5 +6,7 @@ while read line;
 do
     file=$(echo ${line} | awk '{print $1}')
     metadata=$(echo ${line} | awk '{print $2}')
-    aws s3 cp ./static/${line} s3://${S3_BUCKET_NAME}/${line} --metadata "${metadata}"
+    CMD="aws s3 cp ./static/${line} s3://${S3_BUCKET_NAME}/${line} --metadata '${metadata}'"
+    echo "Cmd : ${CMD}"
+    ${CMD}
 done < manifest
