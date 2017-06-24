4f4b751d2a8e2dad518ba6d0dedf51415097d37b
Gabriel Malet <gabrielmalet@gmail.com>
2017-06-24 20:20:31 +0200

    Fix

--- jscraper-ui/scripts/aws-ci.sh
+++ jscraper-ui/scripts/aws-ci.sh
@@ -6,5 +6,5 @@ while read line;
 do
     file=$(echo ${line} | awk '{print $1}')
     metadata=$(echo ${line} | awk '{print $2}')
-    aws s3 cp ./static/${file} s3://${S3_BUCKET_NAME}/${file} --metadata '${metadata}'
+    aws s3 cp ./static/${file} s3://${S3_BUCKET_NAME}/${file} --metadata "'${metadata}'"
 done < manifest
