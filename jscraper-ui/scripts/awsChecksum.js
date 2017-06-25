fe4cdc653588f9963fdca86bb0d30424014a8fe5
Gabriel Malet <gabrielmalet@gmail.com>
2017-06-26 01:49:38 +0200

    Handle exception

--- jscraper-ui/scripts/awsChecksum.js
+++ jscraper-ui/scripts/awsChecksum.js
@@ -5,9 +5,12 @@ const filePath = path.join(process.cwd(), process.argv[2]);
 if (fs.existsSync(filePath)) {
   const contentFile = fs.readFileSync(filePath);
   if (contentFile) {
-    const currMetadata = JSON.parse(contentFile);
-    if (currMetadata && currMetadata.Metadata && currMetadata.Metadata.md5chksum) {
-      console.log(currMetadata.Metadata.md5chksum);
+    try {
+      const currMetadata = JSON.parse(contentFile);
+      if (currMetadata && currMetadata.Metadata && currMetadata.Metadata.md5chksum) {
+        console.log(currMetadata.Metadata.md5chksum);
+      }
+    } catch (Error) {
     }
   }
 }
