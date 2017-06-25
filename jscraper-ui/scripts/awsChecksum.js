c18b662d93913d7a3261df8635983d307dd67e73
Gabriel Malet <gabrielmalet@gmail.com>
2017-06-26 01:42:40 +0200

    Check content file

--- jscraper-ui/scripts/awsChecksum.js
+++ jscraper-ui/scripts/awsChecksum.js
@@ -3,8 +3,11 @@ const fs = require('fs');
 const filePath = path.join(process.cwd(), process.argv[2]);
 
 if (fs.existsSync(filePath)) {
-  const currMetadata = JSON.parse(fs.readFileSync(filePath));
-  if (currMetadata && currMetadata.Metadata && currMetadata.Metadata.md5chksum) {
-    console.log(currMetadata.Metadata.md5chksum);
+  const contentFile = fs.readFileSync(filePath);
+  if (contentFile) {
+    const currMetadata = JSON.parse(contentFile);
+    if (currMetadata && currMetadata.Metadata && currMetadata.Metadata.md5chksum) {
+      console.log(currMetadata.Metadata.md5chksum);
+    }
   }
 }
