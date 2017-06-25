7094501b04307ac024c710c1f7cec43a2042bb0c
Gabriel Malet <gabrielmalet@gmail.com>
2017-06-25 19:55:16 +0200

    Add cache invalidation

--- jscraper-ui/scripts/aws-ci.sh
+++ jscraper-ui/scripts/aws-ci.sh
@@ -6,5 +6,14 @@ while read line;
 do
     file=$(echo ${line} | awk '{print $1}')
     metadata=$(echo ${line} | awk '{print $2}')
-    aws s3 cp ./static/${file} s3://${S3_BUCKET_NAME}/${file} --metadata ${metadata}
+    md5=$(openssl md5 -binary PATH/TO/FILE | base64)
+
+    # Push files
+    aws s3 cp ./static/${file} s3://${S3_BUCKET_NAME}/${file} \
+        --metadata md5chksum=${md5},${metadata} \
+        --content-md5 ${md5}
+
+    # Invalidate Cloud Front Cache
+    aws cloudfront create-invalidation --distribution-id ${CF_DISTRIBUTION_ID} --paths "/*";
+
 done < manifest
