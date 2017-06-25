52427cae5e117ecbae3198545c80a2e02c65ca84
Gabriel Malet <gabrielmalet@gmail.com>
2017-06-26 01:36:36 +0200

    Change 'require' with 'readFileSync'

--- jscraper-ui/scripts/awsChecksum.js
+++ jscraper-ui/scripts/awsChecksum.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filePath = path.join(process.cwd(), process.argv[2]);
 
 if (fs.existsSync(filePath)) {
-  const currMetadata = require(filePath);
+  const currMetadata = JSON.parse(fs.readFileSync(filePath));
   if (currMetadata && currMetadata.Metadata && currMetadata.Metadata.md5chksum) {
     console.log(currMetadata.Metadata.md5chksum);
   }
